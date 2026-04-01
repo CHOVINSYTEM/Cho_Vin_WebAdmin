585b8ec1191fbbd56273c171ad1a9feecc64e7ac
Nguyen Quoc An <nguyenquocan1010@gmail.com>
2026-04-01 11:49:09 +0700

    update firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,3 +1,4 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,24 +6,14 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD5do7PbI3lWpwuT5KSmejah5TimQzK-ng",
-//     authDomain: "deliveryfood-9c436.firebaseapp.com",
-//     projectId: "deliveryfood-9c436",
-//     storageBucket: "deliveryfood-9c436.appspot.com",
-//     messagingSenderId: "682756485581",
-//     appId: "1:682756485581:web:7bcb0b616d335b81285484",
-//     measurementId: "G-0HQRH50TQ3",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAylpn_KS0QugCUnoPu5195s-nWz1CbrKs",
-  authDomain: "boxproduct-ba126.firebaseapp.com",
-  projectId: "boxproduct-ba126",
-  storageBucket: "boxproduct-ba126.appspot.com",
-  messagingSenderId: "293660540157",
-  appId: "1:293660540157:web:0dea29f634285c8736d294",
-  measurementId: "G-G92P56SPTH"
+  apiKey: "AIzaSyCf6j0iDxhykwdsGgigG90ogJHI4TdEOfk",
+  authDomain: "vhgp-bf3e3.firebaseapp.com",
+  projectId: "vhgp-bf3e3",
+  storageBucket: "vhgp-bf3e3.firebasestorage.app",
+  messagingSenderId: "1033937614588",
+  appId: "1:1033937614588:web:da8269a33df1cab164c979",
+  measurementId: "G-MBK5GXCHV5",
 };
 
 // Initialize Firebase
