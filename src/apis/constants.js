730429096d833b51882afd665ab807ec7b98c404
Nguyen Quoc An <nguyenquocan1010@gmail.com>
2026-04-01 16:55:40 +0700

    update

--- src/apis/constants.js
+++ src/apis/constants.js
@@ -1,5 +1,4 @@
-export const BASE_URL_CORAL_TEAM_VERSION =
-  "http://161.118.238.100:8080/api/v1/";
+export const BASE_URL_CORAL_TEAM_VERSION = "/api/proxy/v1/";
 
 export const STORE = "store-management";
 export const PRODUCT = "products";
